38f567a0e13182f8d9a0dc8cf15124940188b6f0
kumkanillam <kumkanillam@gmail.com>
2017-07-04 11:51:07 +0530

    ember-cli upgrade to 2.13.3

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
-/* jshint node: true */
+/* eslint-env node */
 'use strict';
 
 module.exports = {
-  name: 'ember-jquery-sparkline',
+    name: 'ember-jquery-sparkline',
 
-  included: function(app) {
-  	this.app = app;
-    this._super.included(app);
+    included: function(app) {
+        this.app = app;
+        this._super.included(app);
 
-    app.import(app.bowerDirectory + '/jquery.sparkline.dist/dist/jquery.sparkline.js');
-  }
+        app.import(app.bowerDirectory + '/jquery.sparkline.dist/dist/jquery.sparkline.js');
+    }
 };
--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,31 +1,25 @@
-/* jshint node: true */
+/* eslint-env node */
 
 module.exports = function(environment) {
   var ENV = {
     modulePrefix: 'dummy',
     environment: environment,
-    baseURL: '/',
+    rootURL: '/',
     locationType: 'auto',
     EmberENV: {
       FEATURES: {
         // Here you can enable experimental features on an ember canary build
         // e.g. 'with-controller': true
+      },
+      EXTEND_PROTOTYPES: {
+        // Prevent Ember Data from overriding Date.parse.
+        Date: false
       }
     },
 
     APP: {
       // Here you can pass flags/options to your application instance
       // when it is created
-    },
-    contentSecurityPolicy: {
-      'default-src': "'self' *",
-      'script-src': "'self' *",
-      'font-src': "'self' *",
-      'connect-src': "'self' *",
-      'img-src': "'self' *",
-      'style-src': "'self' 'unsafe-inline' *",
-      'media-src': "'self' *",
-      'frame-src': "'self' *"
     }
   };
 
@@ -39,7 +33,6 @@ module.exports = function(environment) {
 
   if (environment === 'test') {
     // Testem prefers this...
-    ENV.baseURL = '/';
     ENV.locationType = 'none';
 
     // keep test console output quieter
