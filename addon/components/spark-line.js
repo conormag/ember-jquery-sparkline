70f602c88d2b65b361aa498531fd4bcb2a62ce1d
kumkanillam <kumkanillam@gmail.com>
2017-07-04 13:43:13 +0530

    ember-cli upgrade 2.13.3 and changed jquery.sparkline.dist to 2.1.3

--- addon/components/spark-line.js
+++ addon/components/spark-line.js
@@ -4,43 +4,42 @@ import layout from '../templates/components/spark-line';
 const { on } = Ember;
 
 export default Ember.Component.extend({
-	layout: layout,
-  tagName: 'div',
+    layout: layout,
+    tagName: 'div',
 
-  classNames: ['sparkline'],
+    classNames: ['sparkline'],
 
 
-  initialize: on('didInsertElement', function() {
+    initialize: on('didInsertElement', function() {
 
-  	var sparkline,
-  			_this = this;
+        var sparkline, _this = this;
 
-  	let options = this.get('options') || {};
-		let data = this.get('data');
-		sparkline = this.$().sparkline(data, options);
+        let options = this.get('options') || {};
+        let data = this.get('data');
+        sparkline = this.$().sparkline(data, options);
 
-		this.$().bind('sparklineClick', function(ev) {
-    	_this.set('sparklineClick', ev.sparklines[0]);
-		});
-		this.$().bind('sparklineRegionChange', function(ev) {
-    	_this.set('sparklineRegionChange', ev.sparklines[0]);
-		});
-  }),
+        this.$().bind('sparklineClick', function(ev) {
+            _this.set('sparklineClick', ev.sparklines[0]);
+        });
+        this.$().bind('sparklineRegionChange', function(ev) {
+            _this.set('sparklineRegionChange', ev.sparklines[0]);
+        });
+    }),
 
-	click() {
-		this.sendAction('action',this.get('sparklineClick'));
-	},
+    click() {
+        this.sendAction('action', this.get('sparklineClick'));
+    },
 
-  mouseMove() {
-  	if (this.get('hover')) {
-  		let sp = this.get('sparklineRegionChange');
-	  	if (sp) {
-		    this.sendAction('hover', sp);
-	  	}
-  	}
-  },
+    mouseMove() {
+        if (this.get('hover')) {
+            let sp = this.get('sparklineRegionChange');
+            if (sp) {
+                this.sendAction('hover', sp);
+            }
+        }
+    },
 
-  destroySparkline: on('willDestroyElement', function() {
-    this.destroy();
-  }),
+    destroySparkline: on('willDestroyElement', function() {
+        this.destroy();
+    }),
 });
