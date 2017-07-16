f69013088420465fff3d7d767a96b2bc6253a4fe
kumkanillam <kumkanillam@gmail.com>
2017-07-17 01:25:48 +0530

    eslint no-undef lint error fix renmaed / path to spark-line

--- addon/components/spark-line.js
+++ addon/components/spark-line.js
@@ -12,17 +12,17 @@ export default Ember.Component.extend({
 
     initialize: on('didInsertElement', function() {
 
-        var sparkline, _this = this;
+        // var sparkline, _this = this;
 
-        let options = this.get('options') || {};
-        let data = this.get('data');
-        sparkline = this.$().sparkline(data, options);
+        // let options = this.get('options') || {};
+        // let data = this.get('data');
+        // sparkline = this.$().sparkline(data, options);
 
-        this.$().bind('sparklineClick', function(ev) {
-            _this.set('sparklineClick', ev.sparklines[0]);
+        this.$().bind('sparklineClick', (ev) => {
+            this.set('sparklineClick', ev.sparklines[0]);
         });
-        this.$().bind('sparklineRegionChange', function(ev) {
-            _this.set('sparklineRegionChange', ev.sparklines[0]);
+        this.$().bind('sparklineRegionChange', (ev) => {
+            this.set('sparklineRegionChange', ev.sparklines[0]);
         });
     }),
 
